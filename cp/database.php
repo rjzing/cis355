6fd8b4d72b1c6ca7ba9b0e84379e395027dfbea4
rjzing <31484043+rjzing@users.noreply.github.com>
2019-04-18 22:11:24 -0400

    Update database.php

--- cp/database.php
+++ cp/database.php
@@ -1,10 +1,10 @@
 <?php
 class Database
 {
-private static $dbName = 'rjzinger';
-private static $dbHost = 'localhost';
-private static $dbUsername = 'rjzinger';
-private static $dbUserPassword = 'rubyred';
+private static $dbName = '';
+private static $dbHost = '';
+private static $dbUsername = '';
+private static $dbUserPassword = '';
 private static $cont=null;
 public function __construct(){
 	die('Init function is not allowed');
